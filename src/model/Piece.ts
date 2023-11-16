08e9673fb54b51e842d4c77eb7949fd660155d93
ninjinski <louis.bailly.pro@gmail.com>
2023-11-16 19:04:41 +0100

    chore: change piece rule

--- src/model/Piece.ts
+++ src/model/Piece.ts
@@ -27,7 +27,7 @@ export class Shooter implements Piece {
   actionZone = new ActionZone({
     moveRange: 1,
     revealRange: 1,
-    killRange: 2,
+    killRange: 1,
     direction: Direction.ORTHOGONAL,
   });
   originSpawnDelta = { dX: 1, dY: 1 };
